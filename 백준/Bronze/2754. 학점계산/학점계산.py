349636e57c21e895498996d8df620db84f9bca8f
Oh-JunTaek <143782929+Oh-JunTaek@users.noreply.github.com>
2024-08-26 22:07:03 +0900

    [Bronze III] Title: 학점계산, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2754. 학점계산/학점계산.py
@@ -0,0 +1,20 @@
+grade_dict = {
+    'A+': 4.3,
+    'A0': 4.0,
+    'A-': 3.7,
+    'B+': 3.3,
+    'B0': 3.0,
+    'B-': 2.7,
+    'C+': 2.3,
+    'C0': 2.0,
+    'C-': 1.7,
+    'D+': 1.3,
+    'D0': 1.0,
+    'D-': 0.7,
+    'F': 0.0
+}
+
+test = input()
+
+if test in grade_dict:
+    print(grade_dict[test])
